5538e555ac7a6425b22c0ec13401d6439457a06d
azkifairuz <azkiajmal@gmail.com>
2024-02-21 23:12:20 +0700

    fix: statistic dashboad component

--- components/StatisticDashboard.vue
+++ components/StatisticDashboard.vue
@@ -1,30 +1,13 @@
 <script setup lang="ts">
-const dataStatistic = ref([
-  {
-    cardIcons: "image/user.svg",
-    cardTitle: "Jumlah Pengguna",
-    cardSubtitle: "12.332",
-  },
-  {
-    cardIcons: "image/at-sign.svg",
-    cardTitle: "Jumlah Daftar Hari",
-    cardSubtitle: "2.234",
-    dataStatistic: 100,
-  },
-  {
-    cardIcons: "image/activity.svg",
-    cardTitle: "Jumlah pengguna aktif",
-    cardSubtitle: "5.122",
-    dataStatistic: -40,
-  },
-]);
+
+const props = defineProps(['dataStatistic']);
 </script>
 
 <template>
   <div>
     <div class="flex gap-6 py-6 pb-4 pt-2 justify-start items-center">
       <StatisticCard 
-      v-for="(statistic, index) in dataStatistic" 
+      v-for="(statistic, index) in props.dataStatistic" 
       :key="index"
       :card-icons="statistic.cardIcons"
       :card-title="statistic.cardTitle" 
--- pages/preview.vue
+++ pages/preview.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
-
+const dataStatistic = ref([
+  {
+    cardIcons: "image/user.svg",
+    cardTitle: "Jumlah Pengguna",
+    cardSubtitle: "12.332",
+  },
+  {
+    cardIcons: "image/at-sign.svg",
+    cardTitle: "Jumlah Daftar Hari",
+    cardSubtitle: "2.234",
+    dataStatistic: 100,
+  },
+  {
+    cardIcons: "image/activity.svg",
+    cardTitle: "Jumlah pengguna aktif",
+    cardSubtitle: "5.122",
+    dataStatistic: -40,
+  },
+]);
 </script>
 
 <template>
   <div>
     <h1>preview</h1>
-    <StatisticDashboard/>
+    <StatisticDashboard :data-statistic="dataStatistic" />
   </div>
 </template>
