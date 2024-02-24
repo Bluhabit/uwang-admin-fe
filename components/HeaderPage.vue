c7b0184ca715dd9cdc8c17907ef47de92ddcada0
maulanafikriash <maulanx835@gmail.com>
2024-02-24 22:12:10 +0700

    fix: margin & icon svg

--- components/HeaderPage.vue
+++ components/HeaderPage.vue
@@ -9,13 +9,13 @@ const formattedDate = computed(() => {
     <header class="bg-white shadow-md">
       <div class="container mx-auto flex justify-between items-center py-4">
         <div class="flex flex-col items-start">
-          <h1 class="text-xl font-bold ml-2">Dashboard</h1>
-          <p class="text-gray-600 ml-2">{{ formattedDate }}</p>
+          <h1 class="text-xl font-bold ml-4">Dashboard</h1>
+          <p class="text-gray-600 ml-4">{{ formattedDate }}</p>
         </div>
         <div class="flex items-center">
           <div class="flex items-center bg-white shadow-md rounded-full p-2 cursor-pointer mr-4">
             <a href="#">
-              <img src="image/bell.png" alt="notifikasi">
+              <img src="image/bell.svg" alt="notifikasi">
             </a>
           </div>
         </div>
