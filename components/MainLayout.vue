cc964cdf09f6e92c9b4d3b23710b4f2a6298f010
maulanafikriash <maulanx835@gmail.com>
2024-02-24 22:01:46 +0700

    fix: MainLayout template

--- components/MainLayout.vue
+++ components/MainLayout.vue
@@ -1,17 +1,34 @@
-<template>
-    <div>
-      <!-- HeaderPage -->
-    </div>
-    <div>
-        <!-- sideBar -->
-    </div>
-    <div>
-        <slot>
+<script setup lang="ts">
+const isSidebarOpen = ref(true);
 
-            <!-- contennya masuk ke slot ini -->
-            <!-- StstisticCard -->
-            <!-- StstisticDashboard -->
-            <!-- DataTable -->
-        </slot> 
+const sidebarContainerClasses = computed(() => ({
+  'w-64': isSidebarOpen.value, 
+  'w-0': !isSidebarOpen.value, 
+}));
+
+const contentContainerClasses = computed(() => ({
+  'w-full': isSidebarOpen.value, 
+  'ml-0': !isSidebarOpen.value,
+}));
+</script>
+
+<template>
+  <div class="flex h-screen">
+    <div :class="sidebarContainerClasses">
+      <!-- <Sidebar/> -->
     </div>
-  </template>
+
+    <div class="flex-1">
+      <div :class="contentContainerClasses">
+        <!-- <HeaderPage/> -->
+        <main class="overflow-y-auto">
+          <slot>
+            <!-- <StatisticDashboard :statistic="dataStatistic" />
+            <h1 class="text-center mt-10 text-xl">Waiting Data table from Azki</h1> -->
+          </slot>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+  
