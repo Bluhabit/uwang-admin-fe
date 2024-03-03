c67d1e0ef938842e546a293867d946446eea1df6
maulanafikriash <maulanx835@gmail.com>
2024-03-03 17:49:38 +0700

    feat: fetch API

---
+++ store/getStatisticCard.ts
@@ -0,0 +1,24 @@
+export const useUserStatisticsStore = defineStore('getStatisticsCard', {
+  state: () => ({
+    statistics: {
+      active_user: '',
+      user_claim: '',
+      total_user: '',
+    },
+    isLoading: true,
+  }),
+  actions: {
+    async fetchStatistics() {
+      this.isLoading = true;
+      try {
+        const response = await fetch('https://api.bluhabit.id/account/v1/admin/get-statistic');
+        const data = await response.json();
+        this.statistics = data.data;
+      } catch (error) {
+        console.error('Error fetching statistics:', error);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
+});
