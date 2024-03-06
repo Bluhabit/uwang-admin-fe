d1365a12158f550b2131d9e96d017e3b14de5cbc
azkifairuz <azkiajmal@gmail.com>
2024-03-06 11:58:58 +0700

    fix: function type

--- store/useTopUser.ts
+++ store/useTopUser.ts
@@ -1,5 +1,5 @@
 
-export const useTopUser = defineStore<string,{topUser:UserCredentialResponse,isLoading:boolean}>('useTopUser',{
+export const useTopUser = defineStore<string,{topUser:UserCredentialResponse,isLoading:boolean},{},{getTopUser:()=>Promise<void>}>('useTopUser',{
   state:() => ({
     topUser:[],
     isLoading: true,
@@ -11,7 +11,6 @@ export const useTopUser = defineStore<string,{topUser:UserCredentialResponse,isL
       const data = await get<Array<UserCredentialResponse>>("uwang/dev/account/v1/admin/get-top-user")
       this.topUser = data.data
       this.isLoading = false
-       
     }
   }
 })
