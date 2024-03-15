df928516c54ec4ab9dad87e4403ce12755fc39d2
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-03-15 21:05:40 +0700

    feat : create access management page

---
+++ components/AccessManagement.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="bg-separator h-screen">
+        <div class="flex justify-between items-center p-4">
+            <div>
+                <h1 class="text-2xl font-bold">Manajemen akses</h1>
+                <p class="text-gray-500">Tambah atau hapus akses setiap role</p>
+            </div>
+            <div>
+                <button class="border-2 border-gray-300 p-2 rounded-lg hover:bg-gray-100"><img src="image/bell.svg"
+                        alt=""></button>
+            </div>
+        </div>
+        <TableRole />
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 const route = useRoute()
 const { get } = useApi()
 
@@ -12,8 +13,9 @@ onMounted(async () => {
 
 <template>
   <div>
+    <AccessManagement />
     <!-- <button @click="auth.singIn()"></button> -->
-    <h1 class="bg-green-500">Nuxt Routing set up successfully!</h1>
+    <!-- <h1 class="bg-green-500">Nuxt Routing set up successfully!</h1>
     <p>Current route: {{ route.path }}</p>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a> -->
   </div>
