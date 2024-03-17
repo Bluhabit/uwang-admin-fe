c45623abf15e3551431bf1d2395cc85f41557b69
azkifairuz <azkiajmal@gmail.com>
2024-03-17 17:18:44 +0700

    fix main layout

--- components/MainLayout.vue
+++ components/MainLayout.vue
@@ -3,28 +3,29 @@ const isSidebarOpen = ref(true);
 
 const sidebarContainerClasses = computed(() => ({
   'w-64': isSidebarOpen.value, 
-  'w-0': !isSidebarOpen.value,
+  'w-12': !isSidebarOpen.value,
 }));
 
-const contentContainerClasses = computed(() => ({
-  'flex-grow': isSidebarOpen.value,
-  // 'ml-64': isSidebarOpen.value,
-  'ml-0': !isSidebarOpen.value,
-}));
+// const contentContainerClasses = computed(() => ({
+//   'w-full': isSidebarOpen.value,
+//   // 'ml-64': isSidebarOpen.value,
+//   'w-full': !isSidebarOpen.value,
+// }));
+function toogleSidebar() {
+    isSidebarOpen.value = !isSidebarOpen.value
+}
 </script>
 
 <template>
   <div class="flex h-screen">
     <div :class="sidebarContainerClasses">
-      <slot name="sidebar"></slot>
+      <Sidebar @toggle-sidebar="toogleSidebar" :is-sidebar-open="isSidebarOpen" />
     </div>
 
-    <div :class="contentContainerClasses">
-      <div >
+    <div class="w-full" >
         <main class="overflow-y-auto">
           <slot name="content"></slot>
         </main>
-      </div>
     </div>
   </div>
 </template>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,60 +1,84 @@
 <script setup lang="ts">
-const isSidebarOpen = ref(false)
+const props = defineProps({
+  isSidebarOpen: {
+    type: Boolean,
+    default: false,
+  },
+});
+const emit = defineEmits(["toggleSidebar"]);
 const user = {
   name: "Marquez Pazeyo",
   avatar: "https://i.pravatar.cc/150?u=uwang@example.com",
   role: "Super Admin",
-}
-const items = [
+};
+const items = ref([
   { text: "Dashboard", href: "/", icon: "/image/home.svg" },
   {
     text: "Manajemen User",
     icon: "/image/users.svg",
     submenu: [
       { text: "Daftar Pengguna", href: "/users/list" },
-      { text: "Log Aktivitas Pengguna", href: "/users/logs" }
+      { text: "Log Aktivitas Pengguna", href: "/users/logs" },
     ],
     show: false,
   },
   { text: "Manajemen Akses", href: "/access", icon: "/image/settings.svg" },
-]
+]);
 
 function toggleSidebar() {
-  isSidebarOpen.value = !isSidebarOpen.value
+  emit("toggleSidebar");
+  
 }
 
 function logout() {
   // Implementasi logout
 }
 
-function toggleDropdown(itemText: string){
-  const item = items.find(i => i.text === itemText)
+function toggleDropdown(itemText: string) {
+  const item = items.value.find((i) => i.text === itemText);
   if (item) {
-    item.show = !item.show
+    item.show = !item.show;
   }
 }
 
-function isActive(item: any){
-  return window.location.pathname === item.href
+function isActive(item: any) {
+  return window.location.pathname === item.href;
 }
 
-function handleItemClick(item: any){
-  if (!item.submenu) return
-  item.show = !item.show
+function handleItemClick(item: any) {
+  if (!item.submenu) return;
+  item.show = !item.show;
 }
 </script>
 
 <template>
-  <div :class="{ 'w-64': isSidebarOpen, 'w-12': !isSidebarOpen }"
-    class="fixed inset-y-0 left-0 bg-white shadow-md h-screen overflow-y-auto flex flex-col transition-width duration-300">
+  <div
+    :class="{ 'w-64': isSidebarOpen, 'w-12': !isSidebarOpen }"
+    class="fixed inset-y-0 left-0 bg-white shadow-md h-screen overflow-y-auto flex flex-col transition-width duration-300"
+  >
     <div class="flex items-center justify-center py-4 mb-8 mt-4 px-4 relative">
-      <img :src="isSidebarOpen ? '/image/Logo-Uwang.svg' : '/image/Logo.svg'" alt="Logo"
-        :class="{ 'w-30': isSidebarOpen, 'h-6 w-6': !isSidebarOpen, 'absolute': !isSidebarOpen, 'transition-all duration-300': true, }"
-        class="h-auto">
-      <button @click="toggleSidebar" class="absolute"
-        :class="{ 'right-4': isSidebarOpen, 'right-3 top-8': !isSidebarOpen }">
-        <span v-if="isSidebarOpen" class="font-bold text-lg"><img src="/image/chevron-left.svg" alt="row-left"></span>
-        <span v-else><img src="/image/chevron-right.svg" alt="row-right"></span>
+      <img
+        :src="isSidebarOpen ? '/image/Logo-Uwang.svg' : '/image/Logo.svg'"
+        alt="Logo"
+        :class="{
+          'w-30': isSidebarOpen,
+          'h-6 w-6': !isSidebarOpen,
+          absolute: !isSidebarOpen,
+          'transition-all duration-300': true,
+        }"
+        class="h-auto"
+      />
+      <button
+        @click="toggleSidebar"
+        class="absolute"
+        :class="{ 'right-4': isSidebarOpen, 'right-3 top-8': !isSidebarOpen }"
+      >
+        <span v-if="isSidebarOpen" class="font-bold text-lg"
+          ><img src="/image/chevron-left.svg" alt="row-left"
+        /></span>
+        <span v-else
+          ><img src="/image/chevron-right.svg" alt="row-right"
+        /></span>
       </button>
     </div>
 
@@ -64,42 +88,58 @@ function handleItemClick(item: any){
           <div
             class="flex items-center justify-between px-3 py-2 cursor-pointer hover:bg-gray-200"
             :class="{ 'text-[#1041B7] font-semibold': isActive(item) }"
-            @click="handleItemClick(item)">
-            <a :href="item.href" class="flex items-center" v-if="!item.submenu">
-              <img :src="item.icon" class="h-6 w-6 mr-3" alt="Icon">
-              <span>{{ item.text }}</span>
+            @click="handleItemClick(item)"
+          >
+            <a v-if="!item.submenu" :href="item.href" class="flex items-center" >
+              <img :src="item.icon" class="h-6 w-6 mr-3" alt="Icon" />
+              <span v-show="isSidebarOpen">{{ item.text }}</span>
             </a>
             <div v-else class="flex items-center justify-between w-full">
               <div class="flex items-center">
-                <img :src="item.icon" class="h-6 w-6 mr-3" alt="Icon">
-                <span>{{ item.text }}</span>
+                <img :src="item.icon" class="h-6 w-6 mr-3" alt="Icon" />
+                <span  v-show="isSidebarOpen">{{ item.text }}</span>
               </div>
-              <span v-if="item.submenu" class="transition-transform transform"
-                :class="{ 'rotate-180': item.show }">
-                <img src="/image/chevron-down.svg" alt="icon-down">
+              <span
+                v-show="isSidebarOpen"
+                v-if="item.submenu"
+                class="transition-transform transform"
+                :class="{ 'rotate-180': item.show }"
+              >
+                <img src="/image/chevron-down.svg" alt="icon-down" />
               </span>
             </div>
           </div>
           <ul v-if="item.submenu && item.show" class="pl-8">
-            <li v-for="subItem in item.submenu" :key="subItem.text"
-              class="px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-black">
+            <li
+              v-for="subItem in item.submenu"
+              :key="subItem.text"
+              class="px-4 py-2 text-gray-800 hover:bg-gray-200 hover:text-black"
+            >
               <a :href="subItem.href">{{ subItem.text }}</a>
             </li>
           </ul>
         </li>
       </ul>
     </nav>
-    
+
     <div class="mt-auto flex justify-center pb-4">
       <div v-if="isSidebarOpen" class="flex items-center space-x-4">
-        <img v-if="user.avatar" :src="user.avatar" class="w-10 h-10 rounded-full object-cover" alt="Profile">
+        <img
+          v-if="user.avatar"
+          :src="user.avatar"
+          class="w-10 h-10 rounded-full object-cover"
+          alt="Profile"
+        />
         <div>
           <div class="text-black font-bold text-sm">{{ user.name }}</div>
           <div class="text-gray-600 text-xs">{{ user.role }}</div>
         </div>
       </div>
-      <button @click="logout" :class="{ 'ml-3': isSidebarOpen, 'ml-0': !isSidebarOpen }">
-        <img src="/image/log-out.svg" class="h-6 w-6" alt="Logout Icon">
+      <button
+        @click="logout"
+        :class="{ 'ml-3': isSidebarOpen, 'ml-0': !isSidebarOpen }"
+      >
+        <img src="/image/log-out.svg" class="h-6 w-6" alt="Logout Icon" />
       </button>
     </div>
   </div>
