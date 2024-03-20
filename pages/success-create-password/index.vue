5facfd29d4f70abff3591ed2c1e79fdb7d8cf926
maulanafikriash <maulanx835@gmail.com>
2024-03-20 12:18:09 +0700

    fix: change file code success pw

--- pages/success-create-password/index.vue
+++ pages/success-create-password/index.vue
@@ -1,26 +1,16 @@
 <script setup lang="ts">
- const fect = () => {
-    console.log("success");
- }
 
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen py-12">
-    <div class="w-full max-w-md px-8 py-4 bg-white shadow-md rounded-lg">
-      <h2 class="text-center text-2xl font-bold">Password berhasil dibuat</h2>
-      <p class="text-center mt-4">Silahkan login menggunakan password yang telah dibuat.</p>
-      <form>
-        <div class="mt-8">
-          <label for="email" class="block mb-2 text-sm font-medium">Email</label>
-          <input type="email" id="email" class="shadow-sm focus:ring-primary-500 focus:border-primary-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Masukkan alamat email">
+    <div class="flex flex-col items-center justify-center min-h-screen py-12">
+        <div class="w-full max-w-lg px-5 py-4 bg-white shadow-lg shadow-t-md rounded-2xl ">
+            <img src="image/maskSuccess.svg" alt="icon-success" class="pb-6 mx-auto">
+            <h2 class="text-center text-2xl font-bold">Password berhasil dibuat</h2>
+            <p class="text-center mt-4 text-neutral-gray-7">Silahkan login menggunakan password yang telah dibuat.</p>
+            <button
+                class="mt-8 w-full px-4 py-3 text-sm font-medium text-white bg-state-primary-main rounded-xl shadow-sm hover:bg-primary-700">Login</button>
         </div>
-        <div class="mt-6">
-          <label for="password" class="block mb-2 text-sm font-medium">Password</label>
-          <input type="password" id="password" class="shadow-sm focus:ring-primary-500 focus:border-primary-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Masukkan password">
-        </div>
-        <button class="mt-8 w-full px-4 py-2 text-sm font-medium text-white bg-primary-600 rounded-md shadow-sm hover:bg-primary-700">Login</button>
-      </form>
+        <img src="image/g18.svg" alt="cloud" class="mt-10">
     </div>
-  </div>
 </template>
