d946806a77a2a9e525de5427dbbef44ccb3ade9f
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-03-21 22:43:27 +0700

    refactor : change bg

--- pages/access-management/admin-signup/index.vue
+++ pages/access-management/admin-signup/index.vue
@@ -2,7 +2,7 @@
     <div class="bg-layoutBackground max-w-full relative">
         <img src="/image/Logo-Uwang.svg" alt="" class="absolute top-0 left-0 mt-4 ml-4 z-10">
         <div class="relative inset-0 overflow-hidden">
-            <img src="/image/bg.svg" alt="" class="w-full h-full object-cover">
+            <img src="/image/bg.svg" alt="" class="w-full h-full object-cover justify-center">
         </div>
         <CardAdminSignup />
     </div>
